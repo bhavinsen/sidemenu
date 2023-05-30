5336ec449280023784a5efb3a2622410eefbbca1
bhavinsen <senbhavin@gmail.com>
2023-05-30 17:24:13 +0530

    json data added

--- src/Components/Sidemenu.js
+++ src/Components/Sidemenu.js
@@ -1,79 +1,15 @@
 import React, { useState } from 'react'
 import Arrow from './assets/icon'
 import { GiMammoth } from "react-icons/gi";
+import { Data } from '../data';
 
 const Sidemenu = () => {
-
+  
   //State
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState('')
 
-  //JSON Data
-  const Data = [
-    {
-      "key": "mammal",
-      "label": "Mammal",
-      "nodes": [
-        {
-          "key": "canidae",
-          "label": "Canidae",
-          "nodes": [
-            {
-              "key": "dog",
-              "label": "Dog",
-              "nodes": [],
-              "url": "https://www.google.com/search?q=dog"
-            },
-            {
-              "key": "fox",
-              "label": "Fox",
-              "nodes": [],
-              "url": "https://www.google.com/search?q=fox"
-            },
-            {
-              "key": "wolf",
-              "label": "Wolf",
-              "nodes": [],
-              "url": "https://www.google.com/search?q=wolf"
-            }
-          ],
-          "url": "https://www.google.com/search?q=canidae"
-        }
-      ],
-      "url": "https://www.google.com/search?q=mammal"
-    },
-    {
-      "key": "reptile",
-      "label": "Reptile",
-      "nodes": [
-        {
-          "key": "squamata",
-          "label": "Squamata",
-          "nodes": [
-            {
-              "key": "lizard",
-              "label": "Lizard",
-              "url": "https://www.google.com/search?q=lizard"
-            },
-            {
-              "key": "snake",
-              "label": "Snake",
-              "url": "https://www.google.com/search?q=snake"
-            },
-            {
-              "key": "gekko",
-              "label": "Gekko",
-              "url": "https://www.google.com/search?q=gekko"
-            }
-          ],
-          "url": "https://www.google.com/search?q=squamata"
-        }
-      ],
-      "url": "https://www.google.com/search?q=reptile"
-    }
-  ]
-
-  return (
+   return (
     <>
       <nav
         id="sidenav-1"
